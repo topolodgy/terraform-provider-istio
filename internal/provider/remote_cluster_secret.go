3eeaa4e98ed3ffd8a6e73110ca67356ed407a348
Topolodgy <topolodgy@gmail.com>
2026-05-28 15:28:03 +0100

    updating to fix fmt

--- internal/provider/remote_cluster_secret.go
+++ internal/provider/remote_cluster_secret.go
@@ -6,8 +6,8 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework-timeouts/resource/timeouts"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
